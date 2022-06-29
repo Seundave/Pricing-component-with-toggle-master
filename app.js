e6399bb1e3f093bb37333f54fd0b6680e109c1ca
David <olayinkadavid62@yahoo.com>
2022-06-29 17:29:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector(".toggle");
+const toggleBtn = document.querySelector(".toggle_btn");
+const Body = document.querySelector("body");
+
+toggle.addEventListener("click", (e) => {
+    toggleBtn.classList.toggle("night-mode")
+    if (toggleBtn.classList.contains("night-mode")){
+        Body.classList.add("night");
+    }else{
+        Body.classList.remove("night"); 
+    }
+});
