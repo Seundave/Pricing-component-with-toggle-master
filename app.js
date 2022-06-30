76569a4ebf71e3f03c21ca3318e51944465d3172
David <olayinkadavid62@yahoo.com>
2022-06-30 11:41:13 +0100

    Removed some style properties and added keyboard functionalities to the toggle.

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const toggle = document.querySelector(".toggle");
 const toggleBtn = document.querySelector(".toggle_btn");
-const annualAmount = document.querySelector(".a-amount");
-const monthAmount = document.querySelector(".m-amount");
-const pannualAmount = document.querySelector(".p-a-amount");
-const pmonthAmount = document.querySelector(".p-m-amount");
-const mannualAmount = document.querySelector(".m-a-amount");
-const mmonthAmount = document.querySelector(".m-m-amount");
+const annualAmount = document.querySelectorAll(".a-amount");
+const monthAmount = document.querySelectorAll(".m-amount");
+
 
 toggle.addEventListener("click", (e) => {
     toggleBtn.classList.toggle("active");
-    annualAmount.classList.toggle("active");
-    monthAmount.classList.toggle("active");
-    pannualAmount.classList.toggle("active");
-    pmonthAmount.classList.toggle("active");
-    mannualAmount.classList.toggle("active");
-    mmonthAmount.classList.toggle("active");
-   
+    for (i = 0; i < annualAmount.length; i++){
+        annualAmount[i].classList.toggle("active");
+        monthAmount[i].classList.toggle("active");
+    }
+    
+});
 
+document.addEventListener("keydown", (e) =>{
+    e.preventDefault();
+    var name = e.code;
+
+    if (name === "Tab"){
+        toggleBtn.classList.toggle("active");
+        for (i = 0; i < annualAmount.length; i++){
+            annualAmount[i].classList.toggle("active");
+            monthAmount[i].classList.toggle("active");
+        }
+    }
+
+    if (name === "Space"){
+        toggleBtn.classList.toggle("active");
+        for (i = 0; i < annualAmount.length; i++){
+            annualAmount[i].classList.toggle("active");
+            monthAmount[i].classList.toggle("active");
+        }
+    }
 });
